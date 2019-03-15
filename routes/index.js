079b3105a4a4dbf34b8fb98b8a4ff8e7ce4ab08b
spencermize <spence.bassman@gmail.com>
2019-03-15 12:30:18 -0400

    start work on supporting multiple files

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
+var fs = require('fs');
+var path = require('path');
 var express = require('express');
 var router = express.Router();
 
 /*GET home page. */
 router.get('/',function(req,res,_next){
-	res.render('index',{ title: 'Warmer' });
+	fs.readdir(path.join(path.dirname(require.main.filename),'nc'),function(err,items){
+		if (err){
+			console.error(err);
+		} else {
+			res.render('index',{ title: 'Warmer',files: items });
+		}
+	});
 });
 
 module.exports = router;
